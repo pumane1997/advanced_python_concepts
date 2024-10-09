5ba407d8bfba978c2930e6cbf1a23282c4e91358
Pratik Mane <pratik.mane@thecrownestate.co.uk>
2024-10-09 22:38:23 +0530

    python objects

---
+++ 1_objects.py
@@ -0,0 +1,41 @@
+#Objects
+
+#Identifiers
+
+#Operators
+
+#Keywords
+
+#Comments
+
+#Blank lines, White space, Indentation
+
+
+'''
+Python objects
+
+In Python, everything is an object. This means that every variable, function, 
+class, or data structure you work with is an object.
+
+All of them have type -> you can check with type(<object>)
+
+    lets say,
+
+    inty = 5    ... now looks very simple but actually is int(x=5)
+
+Lets say you install some module/lib. If you run <module>.__file__ , then you will 
+get the library path. If you go on that path and open it, you'll see all the code of it,
+all the methods and so on. You'll realize that those are classes.
+
+
+The objects int, list etc are also classes, these are designed by python team &
+they are written in C python. C python is the software that we use to run python code
+and all builtin objects are in C language (numpy as well) therefore we cannot view the 
+code of inbuilt objects.
+
+If you want to create any type of objects, you can design them using class keyword.
+
+Tip: first design the objects/classes on paper, then in code
+    
+    
+'''

f453f317caa156274d02b2e2c6f805a9155227ae
Pratik Mane <pratik.mane@thecrownestate.co.uk>
2024-10-10 15:40:38 +0530

    added structured folder and files for freeCodeCamp crash course

---
+++ freeCodeCamp_CrashCourse/datatypes/datatypes.py
@@ -0,0 +1,39 @@
+'''
+Numeric Data Types in Python: Integers, Floats, and Complex
+
+Integers: Integers are numbers without decimals.
+Floats: Floats are numbers with decimals.
+Complex: Complex numbers have a real part and an imaginary part denoted with j. 
+        You can create complex numbers in Python with complex(). 
+        The first argument will be the real part and the second argument will be the 
+        imaginary part.
+
+'''
+
+'''
+Strings 
+
+They contain a sequence of characters and they are usually used to represent 
+text in the code.
+
+Quotes Within Strings
+If we define a string with double quotes "", then we can use single quotes within 
+the string. For example:
+"I'm 20 years old"
+
+String Indexing
+We can use indices to access the characters of a string in our Python program. An index is an integer that represents a specific position in the string. They are associated to the character at that position.
+
+This is a diagram of the string "Hello":
+
+String:  H e l l o
+Index:   0 1 2 3 4
+
+'''
+
+
+'''
+
+
+
+'''
---
+++ freeCodeCamp_CrashCourse/datatypes/strings.py
@@ -0,0 +1,21 @@
+
+'''
+Strings 
+
+They contain a sequence of characters and they are usually used to represent 
+text in the code.
+
+Quotes Within Strings
+If we define a string with double quotes "", then we can use single quotes within 
+the string. For example:
+"I'm 20 years old"
+
+String Indexing
+We can use indices to access the characters of a string in our Python program. An index is an integer that represents a specific position in the string. They are associated to the character at that position.
+
+This is a diagram of the string "Hello":
+
+String:  H e l l o
+Index:   0 1 2 3 4
+
+'''

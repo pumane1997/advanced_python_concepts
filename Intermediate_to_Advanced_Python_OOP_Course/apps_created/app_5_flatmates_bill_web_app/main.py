42ac064c4a7be31cd893ac83f962f5c6b0663bdf
Pratik Mane <pumane1997@gmail.com>
2024-11-18 20:31:38 +0530

    created setup for app 5

---
+++ Intermediate_to_Advanced_Python_OOP_Course/apps_created/app_5_flatmates_bill_web_app/main.py
@@ -0,0 +1,22 @@
+from flask.views import MethodView
+from wtforms import Form
+
+class HomePage(MethodView):
+
+    pass
+
+
+class BillFormPage(MethodView):
+
+    pass
+
+
+class ResultPage(MethodView):
+
+    pass
+
+
+class ResultForm(Form):
+
+    pass
+

ce66dd66a9fec4fd3dd0e5f528dc9bbbaac20f0a
Pratik Mane <pumane1997@gmail.com>
2024-12-24 18:01:51 +0530

    refactored app 7

--- Intermediate_to_Advanced_Python_OOP_Course/apps_created/app_7_automated_emails/main.py
+++ Intermediate_to_Advanced_Python_OOP_Course/apps_created/app_7_automated_emails/main.py
@@ -3,6 +3,8 @@ import yagmail
 from news import Newsfeed
 from datetime import date, timedelta
 
+'''
+First version
 
 while True: # the scripts executes continuously
 
@@ -22,5 +24,27 @@ while True: # the scripts executes continuously
                             \n See what's on about {row['interest']} today  \
                             \n {news.get()}  \
                             \n Pratik")
+'''
+
+def send_emails_in_batch(user, password):
+    news = Newsfeed(
+                            interests=row['interest'], 
+                            from_date=(date.today()-timedelta(days=1)),
+                            to_date=(date.today()-timedelta(days=1))
+                            )
+    email = yagmail.SMTP(user=user, password=password)
+    email.send(to=row['email'],
+            subject=f"your {row['interest']} news for today",
+            contents=f"Hi {row['name']}, \
+                    \n See what's on about {row['interest']} today  \
+                    \n {news.get()}  \
+                    \n Pratik")
+
+while True:
+
+    if datetime.datetime.now().hour == 17 and datetime.datetime.now().minute == 45:
+        data = pd.read_excel('people.xlsx')
+        for index, row in data.iterrows():
+            send_emails_in_batch(user="matecolab01@gmail.com", password='tmpazmytpehunmar')
 
 # python ./main.py
--- Intermediate_to_Advanced_Python_OOP_Course/apps_created/app_7_automated_emails/news.py
+++ Intermediate_to_Advanced_Python_OOP_Course/apps_created/app_7_automated_emails/news.py
@@ -14,11 +14,24 @@ class Newsfeed:
         self.to_date = to_date
         self.language = language
 
-    def get(self):
-        url = self.base_url+f"qInTitle={self.interests}&"+f"from={self.from_date}&"+"to={self.to_date}&"+f"language={self.language}&"+f"apiKey={self.api_key}"
+    def _buil_url(self): # this is a private method and is not meant to be used outside of the class
+        url = self.base_url+ \
+              f"qInTitle={self.interests}&"+ \
+              f"from={self.from_date}&"+ \
+              f"to={self.to_date}&"+ \
+              f"language={self.language}&"+ \
+              f"apiKey={self.api_key}"
+        return url
+
+    def _get_article(self):
+        url = self._buil_url()
         response = requests.get(url)
         content = response.json()
         articles = content['articles']
+
+    def get(self):
+        # created separate method for building url & getting article from url
+        # as there is a lot going on in this method
         email_body = ''
         for article in articles:
             email_body = email_body + article['title'] + "\n" + article['url'] + "\n\n"

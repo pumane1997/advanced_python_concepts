0019213d6415c2b1d15aa1b5eb4947046e01da2b
Pratik Mane <pumane1997@gmail.com>
2024-12-31 12:46:13 +0530

    created empty files and classes for app 8

---
+++ Intermediate_to_Advanced_Python_OOP_Course/apps_created/app_8_instant_dictonary/definition.py
@@ -0,0 +1,12 @@
+
+class Definition:
+    ''' 
+        This class represents the definition of a term returned by
+        the dictonnary
+    '''
+
+    def __init__(self, term):
+        term = self.term
+
+    def get(self):
+        pass 
